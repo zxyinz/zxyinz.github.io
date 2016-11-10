a4dcd77d3a8e3be821ff3b2d982ee208479e753f
zxyinz <zxyinz@gmail.com>
2016-11-10 12:58:11 -0500

    Add Sandbox p2

---
+++ Sandbox/Sandbox/Unicom/script/myTODO.js
@@ -0,0 +1,78 @@
+﻿function getMyTOTOInfo()
+{
+    var url = "/queryPortalIndexMeetingAndSMS.do";
+    var params = "";
+    $.ajax(
+    {
+        type: "POST",
+        url: url,
+        data: params,
+        success: function (msg)
+        {
+            var arr = msg.split('~');
+            var verifyCount = arr[0];
+            var examCount = arr[1];
+            var courseCount = arr[2];
+            var feedBackCount = arr[3];
+            var meetingCount = arr[4];
+            var smsCount = arr[5];
+            var bugCount = arr[6];
+
+            document.getElementById('verifyCount').innerText = '(' + verifyCount + ')';
+            document.getElementById('examCount').innerText = '(' + examCount + ')';
+            document.getElementById('courseCount').innerText = '(' + courseCount + ')';
+            document.getElementById('feedBackCount').innerText = '(' + feedBackCount + ')';
+            document.getElementById('meetingCount').innerText = '(' + meetingCount + ')';
+            document.getElementById('smsCount').innerText = '(' + smsCount + ')';
+            document.getElementById('bugCount').innerText = '(' + bugCount + ')';
+
+        }
+    });
+}
+
+function openSmsInfo(sParam)
+{
+    //alert(sParam);		
+    var time = new Date();
+    var ret = window.showModalDialog('/elnsysmanage/sysSmsMain.jsp?userid=' + sParam + "&" + time, window, 'dialogWidth:780px;dialogHeight:460px;center:yes;scroll:yes;help:no;resizable:no;status:no');
+}
+
+function modifyPassword()
+{
+    window.showModalDialog("/showmypasswordmodify.do", window, "dialogWidth:780px;dialogHeight:460px;center:yes;scroll:yes;help:no;resizable:no;status:no");
+}
+function loadWaitingVerifyPage()
+{
+    //window.showModalDialog("../portal/verifyPage.jsp", window,"dialogWidth:780px;dialogHeight:460px;center:yes;scroll:yes;help:no;resizable:yes;status:yes");
+    window.open("../verifyPage.jsp", "verifyPage", "height=" + screen.height + "px, width=" + screen.width + "px, top=0px, left=0px, scrollbars=yes, resizable=no, location=no, status=yes");
+
+}
+function loadExamPage()
+{
+    window.open("../myExamPage.jsp", "verifyPage", "height=" + screen.height + "px, width=" + screen.width + "px, top=0px, left=0px, scrollbars=yes, resizable=no, location=no, status=yes");
+
+}
+function loadCoursePage()
+{
+    window.open("../myCoursePage.jsp", "verifyPage", "height=" + screen.height + "px, width=" + screen.width + "px, top=0px, left=0px, scrollbars=yes, resizable=no, location=no, status=yes");
+
+}
+function loadFeedBackPage()
+{
+    window.open("../myfeedBackPage.jsp", "verifyPage", "height=" + screen.height + "px, width=" + screen.width + "px, top=0px, left=0px, scrollbars=yes, resizable=no, location=no, status=yes");
+
+}
+function loadBugPage()
+{
+    window.open("myBugPage.jsp", "verifyPage", "height=" + screen.height + "px, width=" + screen.width + "px, top=0px, left=0px, scrollbars=yes, resizable=no, location=no, status=yes");
+}
+
+function loginmeeting()
+{
+    var time = new Date();
+    var url = "../../eln_livechat/meetingList.jsp";
+    window.open(url, "meeting", "height=" + screen.height + "px, width=" + screen.width + "px, top=0px, left=0px, scrollbars=yes, resizable=no, location=no, status=yes");
+    //var time=new Date();	
+    //var url="/eln_livechat/meetingList.jsp";
+    //window.open(url,"meeting","modal=yes,width="+window.screen.width/2+",height="+window.screen.height/2+",resizable=no,scrollbars=no,status=no,toolbar=no,menubar=no");	
+}
---
+++ Sandbox/Sandbox/Unicom/script/ui.js
@@ -0,0 +1,31 @@
+﻿// JavaScript Document
+$(function () {
+    $(".tabShow .tabCaption li").click(
+		function () {
+		    $(this).addClass("on").siblings().removeClass("on");
+		    var index = $(this).index()
+		    $(this).parents(".tabShow").find(".tabContent").children(".tabBox").eq(index).addClass("on").siblings().removeClass("on");
+		})
+
+    $(".nav li").click(
+		function () {
+		    $(this).addClass("open").siblings().removeClass("open");
+		    $(".nav li a").removeClass("a_red");
+		    $(this).children().eq(0).addClass("a_red");
+		})
+})
+
+// index广告滚动图
+$(function () {
+    var box = $('.main-c-3');
+    if (box && box.length >= 1) {
+        box.iFadeSlide({
+            field: $('.main-c-3 a'),
+            icocon: $('.ico'),
+            hoverCls: 'high',
+            curIndex: 0,
+            inTime: 1000,
+            interval: 5000
+        })
+    }
+})
